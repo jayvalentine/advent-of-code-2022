11d843eb92a731373b1e5f2a0e9c242fdd8d0479
Jay Valentine <jayv136@gmail.com>
2022-12-01 17:27:58 +0000

    Solving day1 part2

--- day1.rb
+++ day1.rb
@@ -9,11 +9,21 @@ def get_food_carried(entries)
             this_food << e.to_i
         end
     end
+
+    # Add the last elf's food to the list.
+    foods << this_food unless this_food.empty?
+
     foods
 end
 
-example_foods = get_food_carried(File.read("data/day1/example1.txt").split("\n"))
+example_foods = get_food_carried(File.read("data/day1/example.txt").split("\n"))
 puts "Example 1: #{example_foods.map(&:sum).max}"
 
-puzzle1_foods = get_food_carried(File.read("data/day1/puzzle1.txt").split("\n"))
-puts "Puzzle 1: #{puzzle1_foods.map(&:sum).max}"
+puzzle_foods = get_food_carried(File.read("data/day1/puzzle.txt").split("\n"))
+puts "Puzzle 1: #{puzzle_foods.map(&:sum).max}"
+
+example_top_three = example_foods.map(&:sum).sort.last(3)
+puts "Example 2: #{example_top_three.sum}"
+
+puzzle_top_three = puzzle_foods.map(&:sum).sort.last(3)
+puts "Puzzle 2: #{puzzle_top_three.sum}"
