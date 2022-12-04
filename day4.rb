22b30141213620745860117fd251c7f0795baf03
Jay Valentine <jayv136@gmail.com>
2022-12-04 08:39:32 +0000

    Solution to day 4, part 2

--- day4.rb
+++ day4.rb
@@ -8,12 +8,26 @@ def parse_range(s)
     start..finish
 end
 
-def part1(input)
-    ranges = input.split("\n").map do |s|
+def get_ranges(input)
+    input.split("\n").map do |s|
         s.split(',').map { |s| parse_range(s) }
     end
+end
+
+def part1(input)
+    ranges = get_ranges(input)
 
     ranges.count { |pair| pair[0].cover?(pair[1]) || pair[1].cover?(pair[0]) }
+end
+
+def ranges_overlap(r1, r2)
+    r1.cover?(r2.begin) || r1.cover?(r2.end) || r2.cover?(r1.begin) || r2.cover?(r1.end)
+end
+
+def part2(input)
+    ranges = get_ranges(input)
+
+    ranges.count { |pair| ranges_overlap(pair[0], pair[1]) }
 end
 
 AoC::example(day: 4, part: 1, expected: 2) do |input|
@@ -22,4 +36,12 @@ end
 
 AoC::solution(day: 4, part: 1) do |input|
     part1(input)
+end
+
+AoC::example(day: 4, part: 2, expected: 4) do |input|
+    part2(input)
+end
+
+AoC::solution(day: 4, part: 2) do |input|
+    part2(input)
 end
